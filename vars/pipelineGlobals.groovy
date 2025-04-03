a7986de61436f6db71aa14073f45f46eaf1aeabf
sandeepanainala <nainala_sandeep@yahoo.com>
2025-04-03 12:35:27 +0530

    Jenkins shared library - descision pipeline groovy and global pipeline groovy with the backend jenkinsfile shared lib declared

---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,11 @@
+static final String region(){
+    return "us-east-1"
+}
+
+static final String nexusURL(){
+    return "db-dev.devops91.cloud"
+}
+
+static final String account_id(){
+    return "315069654700"
+}
